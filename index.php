9af18b82cf3ca19982f5935a6298937a4563e7ed
matheus souza <ruivorobado@gmail.com>
2021-01-12 20:19:16 -0300

    meta tags adicionadas e fonte

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+    <!--meta tags-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="marketing digital,agência,automação de marketing">
+    <meta name="author" content="matheus programador">
+    <!--fim meta tags-->
+
+    <!--inicio fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <!--fim fonts-->
+
+    <!--css-->
     <link rel="stylesheet" href="assets/css/resets.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Document</title>
+   <!--fim css-->
+    <title>Agência de Marketing Digital</title>
 </head>
 <body>
-
+teste font
 
 
 <script src="assets/js/functions.js"></script>
