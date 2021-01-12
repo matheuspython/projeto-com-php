7a6f36110761dcc8cb58e2c844a9489838c2e920
matheus souza <ruivorobado@gmail.com>
2021-01-12 20:03:41 -0300

    adicionando a estrutura inicial do projeto php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/resets.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<script src="assets/js/functions.js"></script>
+</body>
+</html>
